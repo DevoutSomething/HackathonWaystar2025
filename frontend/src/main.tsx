f2f27f6055048a0aaf8483ccac838bdcb0b27a7d
lukeeskinner <lukeskinner101@gmail.com>
2025-11-20 10:41:16 -0700

    added tailwind + shadcn

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './index.css'
 import Landing from './pages/landing.tsx'
 import Input from './pages/inputs.tsx'
 import Dashboard from './pages/dashboard.tsx'
